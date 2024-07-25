3c8c45fc29f4ff387ae502bebdde0355450aefea
KayGV <khuongvudang25@gmail.com>
2024-07-25 10:10:57 +0700

    [ADD] user model

---
+++ app/models/user.model.js
@@ -0,0 +1,44 @@
+const sequel = require('sequelize');
+const mySequel = require('../utils/sequelize.util');
+
+const account = mySequel.define('account', {
+    id: {
+        type: sequel.BIGINT(20),
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    username: {
+        type: sequel.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: sequel.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: sequel.STRING,
+        allowNull: true,
+    },
+    created_at: {
+        type: sequel.DATE,
+        allowNull: true,
+        defaultValue: sequel.NOW
+    },
+    updated_at: {
+        type: sequel.DATE,
+        allowNull: true,
+        defaultValue: sequel.NOW
+    },
+}, {
+    underscored: false,
+    timestamps: false,
+    updatedAt: false,
+    createdAt: false,
+    includeDeleted: true,
+    paranoid: true,
+    freezeTableName: true,
+    tableName: 'users',
+});
+
+module.exports = account;
